f107584f078ab03fd0f31e847f84d64c4eed64de
BassamAlim <65797540+BassamAlim@users.noreply.github.com>
2022-02-12 22:00:57 +0300

    added my part of neighbors

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import json
+import xlrd
+import pandas as pd
+import openpyxl
 
-
-with open('Cities.json', encoding='utf-8') as file:
+"""with open('Cities.json', encoding='utf-8') as file:
     cities = json.load(file)
 
-
-name = text[0]['name']
-print(name)
-
-
-def goal_test():
-    return True
+name = cities[0]['name']
+print(name)"""""
 
 
+loc = "neighbors.xlsx"
 
+"""wb = xlrd.open_workbook_xls(loc)
+sheet = wb.sheet_by_index(0)
+print(sheet.cell_value(0, 0))"""""
 
-
+data = pd.read_excel(loc)
+#df = pd.DataFrame(data, )
+#print(df)
+print(data.loc[0, 0])
